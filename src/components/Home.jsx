9f5aaee48bf7fcde005136351b01dfb05cecc9bc
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2024-12-04 23:01:33 +0600

    Added home page sidebar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,41 @@
-
-import { NavLink } from 'react-router-dom';
+import userIcon from "../assets/profilePicture.png";
+import homeIcon from "../assets/homeIcom.png";
+import messageIcon from "../assets/messageIcon.png";
+import notificationIcon from "../assets/notificationIcon.png";
+import settingIcon from "../assets/settingsIcon.png";
+import logoutIcon from "../assets/logoutIcon.png";
 
 const Home = () => {
-    return (
-        <div>
-            <h1 className='text-center text-5xl text-green-500 my-32'>Welcome to Homepage</h1>
-            
-            <div className='text-center'>
-            <NavLink to='/registration' className='bg-slate-700 font-semibold text-white mx-auto px-6 py-4 rounded-xl hover:bg-slate-400 mb mr-6'>Registration</NavLink>
-            <NavLink to='/login' className='bg-slate-700  font-semibold text-white mx-auto px-6 py-4 rounded-xl hover:bg-slate-400 '>Login</NavLink>
-
-            </div>
-            
-            
+  return (
+    <div className="m-8 grid gap-10 grid-cols-12">
+      <div className="col-span-2 bg-[#5F35F5] rounded-[20px] flex flex-col  ">
+        <div className="mt-[38px] flex justify-center mb-16">
+          <img src={userIcon} alt="" />
         </div>
-    );
+        <div className="relative ml-[70px] rounded-l-[20px] mb-16 bg-white pt-5 pb-6 pl-11 pr-[70px] w-[75%]">
+          <img src={homeIcon} alt="" />
+          <div className="absolute top-0 right-[1px] w-2 h-full bg-[#5F35F5] rounded-l-[25px]"></div>
+        </div>
+        <div className="flex justify-center mb-[64px]">
+          <img src={messageIcon} alt="" />
+        </div>
+        <div className="flex justify-center mb-[64px]">
+          <img src={notificationIcon} alt="" />
+        </div>
+        <div className="flex justify-center mb-28">
+          <img src={settingIcon} alt="" />
+        </div>
+        <div className="flex justify-center mb-10">
+          <img src={logoutIcon} alt="" />
+        </div>
+      </div>
+      <div className="col-span-10 border gap-5 border-red-600 grid grid-cols-10">
+        <div className="col-span-4 border border-red-600 "></div>
+        <div className="col-span-3 border border-red-600 "></div>
+        <div className="col-span-3 border border-red-600 "></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
